dba4e8ad666a4205979d18a32d008b5954075c74
zacher6729 <jake.zacher@gmail.com>
2026-04-19 23:17:41 -0500

    Fix import paths in tools pages

--- pages/tools/index.jsx
+++ pages/tools/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Nav from '../components/Nav';
-import { useSleeper } from '../hooks/useSleeper';
+import Nav from '../../components/Nav';
+import { useSleeper } from '../../hooks/useSleeper';
 
 const TOOLS = [
   {
